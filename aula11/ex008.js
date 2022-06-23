4a552450e47e2b893eb57f7b63a7e7083221ad35
ELIAS FONSECA MARTINS <eliasmartinsdev8@gmail.com>
2022-06-23 20:28:31 -0300

    att

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+vel=50.5
+console.log(` A velocidade do seu carro e ${vel}km/h`)
+if(vel>60 ){
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de seguraça!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+pais='EUA'
+console.log(`vivendo em ${pais}`)
+if(pais=='Brasil'){
+  console.log('Vc e Brasileiro!')}
+else{
+  console.log('Vc e Estrangeiro')
+}
